8ad66774d43317213b04439f42112a8f52e2cd57
Michael Bowen <mikee.bowen@gmail.com>
2017-04-07 23:20:38 -0700

    docs(foodBankRoutes): update apiDoc comments

--- api/foodBanks/foodBanksRoutes.js
+++ api/foodBanks/foodBanksRoutes.js
@@ -6,15 +6,25 @@ const router = express.Router();
 const getFoodBank = require('./getFoodBank/getFoodBank');
 
 /**
- * @api {get} /food-banks/closest Request closest food bank
+ * @apiName getFoodBank
+ * @api {get} /food-banks/closest/?latitude=LATITUDE_VALUE&longitude=LONGITUDE_VALUE request closest food bank
  * @apiGroup food banks
  * @apiVersion 1.0.0
- * @apiParam {String} latitude Mandatory value. Submitted as a query string and formatted in signed degree format.
- * @apiParam {String} longitude Mandatory value. Submitted as a query string and formatted in signed degree format.
- * @apiExample Example usage:
+ * @apiParam {String} latitude Mandatory. User's latitude to query by. Submitted as a query string and formatted in signed degree format.
+ * @apiParam {String} longitude Mandatory. User's longitude to query by. Submitted as a query string and formatted in signed degree format.
+ * @apiExample Example Usage:
  * http://example.com/food-banks/closest/?latitude=47.673554&longitude=-122.387062
- * @apiSuccess {JSON} returns  On success returns JSON object containing closest food bank
- * @apiSuccessExample {json} Success-Response:
+ * @apiSuccess {String} address food bank's address
+ * @apiSuccess {String} city_feature the category title
+ * @apiSuccess {String} common_name the name of the food bank
+ * @apiSuccess {String} latitude food bank's latitude
+ * @apiSuccess {String} longitude food bank's longitude
+ * @apiSuccess {Object} location object containing location data for the food bank
+ * @apiSuccess {String} location.type the location type, Point, Polygon, or Line
+ * @apiSuccess {Number[]} location.coordinates array containing point coordinates
+ * @apiSuccess {String} website website address for the food bank
+ * @apiSuccess {Number} distance the distance in miles (in a straight line) from the location submitted to the food bank
+ * @apiSuccessExample {json} Example Success Response:
  *    {
  *     "address": "7005 24th Ave NW",
  *     "city_feature": "Food Banks",
@@ -31,9 +41,9 @@ const getFoodBank = require('./getFoodBank/getFoodBank');
  *     "website": "http://www.ballardfoodbank.org",
  *     "distance": 0.4174263197848783
  *   }
- * @apiError (Error 400) {JSON} returns With an invalid or missing query string returns JSON object with "error" key
- * and description of the error
- * @apiErrorExample {JSON} Error-Response:
+ * @apiError (Error 400) {String} error
+ * error message
+ * @apiErrorExample {JSON} Error Response:
  *    {
  *      "error": "Invalid or missing query string"
  *    }
