dce9c9ee2c68622dbf64205ab50eafd2da944c3d
Michael Bowen <mikee.bowen@gmail.com>
2017-04-08 22:34:28 -0700

    docs(*): rebuild apiDoc and jsdoc websites

--- doc/api_project.js
+++ doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-04-09T05:06:24.689Z",
+    "time": "2017-04-09T05:25:38.581Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
