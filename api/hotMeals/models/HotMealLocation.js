91e6ea422b9589a1fe908fbec080586935d1bc5f
Michael Bowen <mikee.bowen@gmail.com>
2017-05-31 19:31:15 -0700

    test(HotMealLocation): add tests to HotMealLocation

--- api/hotMeals/models/HotMealLocation.js
+++ api/hotMeals/models/HotMealLocation.js
@@ -1,6 +1,23 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const hotMealLocationSchema = require('../schemas/hotMealLocationSchema');
+const hotMealLocationSchema = new mongoose.Schema({
+  day_time: String,
+  location: String,
+  meal_served: String,
+  name_of_program: {
+    type: String,
+    required: true
+  },
+  people_served: String,
+  latitude: {
+    type: String,
+    required: true
+  },
+  longitude: {
+    type: String,
+    required: true
+  }
+});
 
 module.exports = exports = mongoose.model('HotMealLocation', hotMealLocationSchema);
---
+++ api/hotMeals/models/HotMealLocation.test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+const Mockgoose = require('mockgoose').Mockgoose;
+const mockgoose = new Mockgoose(mongoose);
+const expect = require('chai').expect;
+
+const HotMealLocation = require('./HotMealLocation');
+
+describe('HotMealLocation Model', function() {
+  //extend timeout for mockgoose
+  this.timeout(120000);
+
+  before(function(done) {
+    process.env.SOCRATA_DATA_API_KEY = 'fakeApiKey';
+
+    mockgoose.prepareStorage().then(() => {
+      mongoose.connect('mongodb://example.com/TestingDB', (err) => {
+        if (err) done(err);
+        done();
+      });
+    });
+  });
+
+  it('should require name_of_program', function (done) {
+    const testHotMealLocation = new HotMealLocation({
+      latitude: 1,
+      longitude: 2
+    });
+
+    testHotMealLocation.save((err) => {
+      expect(err).to.exist;
+      expect(err.message).to.equal('HotMealLocation validation failed: name_of_program: Path `name_of_program` is required.');
+      done();
+    });
+  });
+
+  it('should require latitude', function (done) {
+    const testHotMealLocation = new HotMealLocation({
+      name_of_program: 'test food bank',
+      longitude: 2
+    });
+
+    testHotMealLocation.save((err) => {
+      expect(err).to.exist;
+      expect(err.message).to.equal('HotMealLocation validation failed: latitude: Path `latitude` is required.');
+      done();
+    });
+  });
+
+  it('should require longitude', function (done) {
+    const testHotMealLocation = new HotMealLocation({
+      name_of_program: 'test food bank',
+      latitude: 2
+    });
+
+    testHotMealLocation.save((err) => {
+      expect(err).to.exist;
+      expect(err.message).to.equal('HotMealLocation validation failed: longitude: Path `longitude` is required.');
+      done();
+    });
+  });
+
+  // restoring everything back
+  after( (done) => {
+
+    mockgoose.prepareStorage()
+      .then(() => {
+        mockgoose.helper.reset().then(() => {
+          mongoose.connection.close((err) => {
+            if (err) done(err);
+            done();
+          });
+        });
+      });
+  });
+});
